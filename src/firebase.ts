0fbaf8cefda5123184c9460b337a4e847ba0693c
오유진 <omh232323@gmail.com>
2023-02-02 15:54:46 +0900

    firebase setup

---
+++ src/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCI4bYArjzf8dx1tR6Ih8OEjQEszOzyYfA",
+  authDomain: "react-twitter-clone-bb2b0.firebaseapp.com",
+  projectId: "react-twitter-clone-bb2b0",
+  storageBucket: "react-twitter-clone-bb2b0.appspot.com",
+  messagingSenderId: "822075061144",
+  appId: "1:822075061144:web:2f41c89388a071c57431ed",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Root from "./Root";
+import { app } from "./firebase";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
+console.log(app);
 
 root.render(
   <React.StrictMode>
